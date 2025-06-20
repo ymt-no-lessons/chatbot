a67fa8723dd4cd26c11c454abad19975f07fbd86
ymt-no-lessons <jpncafe@gmail.com>
2025-06-20 13:25:35 +0900

    Render用にいったん上げなおし

--- app.py
+++ app.py
@@ -92,6 +92,11 @@ def chat():
         replies=data['replies']
     )
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
 
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
