1fb1493ecccd86747b80336ace7c9dde8e31b29a
ymt-no-lessons <jpncafe@gmail.com>
2025-07-24 12:07:33 +0900

    Add files via upload

--- sisa_reply.py
+++ sisa_reply.py
@@ -13,35 +13,24 @@ def check_special_words(user_message):
     # ヒットしなければNone
     return None
 
-    # ア、ウン画像で返答の特定ワードもこっちに書くやりかた
-def reply_aun_stamp():
-    stamp_images = [
-        {"type": "image", "content": "images/sisa/sisa_stamp_aun.png"},
-        {"type": "image", "content": "images/sisa/sisa_stamp_aun2.png"},
-    ]
-    return random.choice(stamp_images)
 
 
 def reply(user_message):
     # 空メッセージ対策
     if not user_message:
-        return {"type": "text", "content": "エッ…"}
+        return {"type": "text", "content": "元気ですかー"}
     
     # specialワードで返答
     special = check_special_words(user_message)
     if special:
         return special
     
-    # ア、ウン画像で返答の特定ワードもこっちに書くやりかたをこっちで返す
-    if any(word in user_message for word in ["合格", "おめでとう", "受かった"]):
-        return reply_aun_stamp()   # 引数いらない！
-    
     # 句読点や空白を除いた最後の1文字
     text = re.sub(r'[\s。、！!？?]+$', '', user_message)
-    if len(text) == 1:
-        return {"type": "text", "content": text + "…"}
-    elif len(text) == 2:
-        return {"type": "text", "content": text + "…"}
+    if len(text) == 3:
+        return {"type": "text", "content": text + "!"}
+    elif len(text) == 4:
+        return {"type": "text", "content": text + "!"}
     
     # 通常パターン（ランダム返し）
     patterns = [
